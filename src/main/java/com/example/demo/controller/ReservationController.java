dff075d1cdb13c457708f10b750aac21565f38b9
RYSIU <krzrys@interia.pl>
2021-04-27 23:47:35 +0200

    updating reservations

--- src/main/java/com/example/demo/controller/ReservationController.java
+++ src/main/java/com/example/demo/controller/ReservationController.java
@@ -7,6 +7,7 @@ import com.example.demo.repositories.ReservationRepository;
 import com.example.demo.repositories.RoomRepository;
 import com.example.demo.repositories.UnavailableTermRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
--- src/main/java/com/example/demo/model/reservation/CheckAndSetDate.java
+++ src/main/java/com/example/demo/model/reservation/CheckAndSetDate.java
@@ -5,46 +5,93 @@ import com.example.demo.model.unavailableTerm.UnavailableTerm;
 import com.example.demo.model.room.Room;
 import com.example.demo.repositories.RoomRepository;
 import com.example.demo.repositories.UnavailableTermRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 @Component
 public class CheckAndSetDate {
 
+    private RoomRepository roomRepository;
+    private boolean wasAnotherReservationAdded = false;
+    int i = 0;
 
-    public UnavailableTerm isRoomAvailable(Room room, RoomRepository roomRepository, Reservation reservation, UnavailableTermRepository unavailableTermRepository) {
+    @Autowired
+    public CheckAndSetDate(RoomRepository roomRepository) {
+        this.roomRepository = roomRepository;
+    }
+
+    public UnavailableTerm isRoomAvailable(Room room, RoomRepository roomRepository, Reservation reservation) {
         LocalDate reservationDayStart = reservation.getReservationDayStart();
         LocalDate reservationDayEnd = reservation.getReservationDayEnd();
         List<UnavailableTerm> unavailableTerms = roomRepository.getOne(room.getId()).getUnavailableTerms();
+        List<UnavailableTerm> dateToCompare = new ArrayList<>();
         for (UnavailableTerm unavailableTerm : unavailableTerms) {
             if (unavailableTerm.getStartOfUnavailableTerm().isAfter(reservationDayStart)) {
                 if (!reservationDayEnd.isBefore(unavailableTerm.getStartOfUnavailableTerm())) {
-                    return null;
+                    dateToCompare.add(unavailableTerm);
                 }
-            } else if (unavailableTerm.getStartOfUnavailableTerm().isBefore(reservationDayStart)) {
+            }
+            if (unavailableTerm.getStartOfUnavailableTerm().isBefore(reservationDayStart)) {
                 if (!reservationDayStart.isAfter(unavailableTerm.getEndOfUnavailableTerm())) {
-                    return null;
+                    dateToCompare.add(unavailableTerm);
                 }
-            } else if (unavailableTerm.getStartOfUnavailableTerm().isEqual(reservationDayStart)) {
+            }
+
+            if (unavailableTerm.getStartOfUnavailableTerm().isEqual(reservationDayStart)) {
+                dateToCompare.add(unavailableTerm);
+            }
+        }
+
+        if (dateToCompare.size() > 0) {
+            int dateWithTheLeastAmountOfPeople = compareDate(dateToCompare);
+            System.out.println(dateWithTheLeastAmountOfPeople);
+            boolean arePlacesAvailable = arePlacesAvailable(reservation, dateWithTheLeastAmountOfPeople);
+            if (!arePlacesAvailable)
                 return null;
+            else {
+                UnavailableTerm unavailableTerm = createUnavailableTerm(reservationDayStart, reservationDayEnd);
+                setDateOnRoomWhenAnotherReservation(dateWithTheLeastAmountOfPeople, unavailableTerm, room, reservation);
+                return unavailableTerm;
             }
         }
         UnavailableTerm unavailableTerm = createUnavailableTerm(reservationDayStart, reservationDayEnd);
-        setDatesOnRoom(roomRepository, unavailableTerm, room, reservation);
+        setDateOnRoomWhenFirstReservation(unavailableTerm, room, reservation);
         return unavailableTerm;
+
+    }
+
+    private int compareDate(List<UnavailableTerm> dateToCompare) {
+        return dateToCompare.stream()
+                .flatMapToInt(x -> IntStream.of(x.getPlacesAvailable()))
+                .min().getAsInt();
+    }
+
+    private boolean arePlacesAvailable(Reservation reservation, int dateWithTheLeastAmountOfPeople) {
+        return dateWithTheLeastAmountOfPeople - reservation.getHowManyPeople() > 0;
     }
 
     private UnavailableTerm createUnavailableTerm(LocalDate reservationDayStart, LocalDate reservationDayEnd) {
         return new UnavailableTerm(reservationDayStart, reservationDayEnd);
     }
 
-    public void setDatesOnRoom(RoomRepository roomRepository, UnavailableTerm unavailableTerm, Room room, Reservation reservation) {
+    public void setDateOnRoomWhenFirstReservation(UnavailableTerm unavailableTerm, Room room, Reservation reservation) {
         Room roomFromDataBase = roomRepository.getOne(room.getId());
         roomFromDataBase.addUnavailableTerm(unavailableTerm);
         unavailableTerm.setRoom(roomFromDataBase);
         unavailableTerm.setPlacesAvailable(room.getForHowManyPeople() - reservation.getHowManyPeople());
+    }
+
+    private void setDateOnRoomWhenAnotherReservation(int dateWithTheLastAmountOfPeople, UnavailableTerm unavailableTerm, Room room, Reservation reservation) {
+        Room roomFromDataBase = roomRepository.getOne(room.getId());
+        roomFromDataBase.addUnavailableTerm(unavailableTerm);
+        unavailableTerm.setRoom(roomFromDataBase);
+        unavailableTerm.setPlacesAvailable(dateWithTheLastAmountOfPeople - reservation.getHowManyPeople());
+
     }
 }
 
